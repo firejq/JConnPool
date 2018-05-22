194744be8e4baf1aff0e96044ddab14ad445f306
firejq <firejq@firejq.com>
2018-05-22 15:22:24 +0800

    Finish Cglib Proxy

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # JConnPool
 
-A toy JDBC connection pool.
+A toy JDBC connection pool simulating MyBatis PooledDataSource.
 
 ## Feature
 
 - [x] 通过动态代理支持连接对象自动回收
   - [x] 动态代理支持 JDK Proxy 方式
-  - [] 动态代理支持 Cglib 方式
-- [] 支持超时设置
-- [] 支持连接池自动扩容
-- [] 支持多线程并发环境
+  - [x] 动态代理支持 Cglib 方式
+- [ ] 支持连接池自动扩容、缩容
+- [ ] 支持使用 JavaBean 配置
 
 ## License
 
--- src/main/java/com/firejq/ProxyUtil.java
+++ src/main/java/com/firejq/ProxyUtil.java
@@ -3,10 +3,7 @@ package com.firejq;
 import net.sf.cglib.proxy.Callback;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.NoOp;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.util.LinkedList;
@@ -18,56 +15,86 @@ import java.util.LinkedList;
  * @author <a href="mailto:firejq@outlook.com">firejq</a>
  */
 public class ProxyUtil {
+	/**
+	 * 默认以cglib方式获取动态代理，供工具类外部使用
+	 * @param conn
+	 * @param pool
+	 * @return
+	 */
 	public static Connection getProxyConnection(Connection conn,
 												final LinkedList<Connection> pool) {
-		// JDK Proxy方式
+		return doProxyByCglib(conn, pool);
+	}
+
+	/**
+	 * 支持以JDKProxy方式获取动态代理，供工具类接口外部使用
+	 * @param conn
+	 * @param pool
+	 * @param isJdk
+	 * @return
+	 */
+	public static Connection getProxyConnection(Connection conn,
+												final LinkedList<Connection> pool,
+												Boolean isJdk) {
+		return isJdk ? doProxyByJDK(conn, pool) : doProxyByCglib(conn, pool);
+	}
+
+	/**
+	 * JDK Proxy方式创建动态代理对象实现逻辑，供工具类接口内部调用
+	 * @param conn
+	 * @param pool
+	 * @return
+	 */
+	private static Connection doProxyByJDK(Connection conn,
+										   final LinkedList<Connection> pool) {
 		return (Connection) Proxy.newProxyInstance(
 				conn.getClass().getClassLoader(),
 				conn.getClass().getInterfaces(),
 				(proxy, method, args) -> {
 					Object res = null;
 					if (!method.getName().equals("close")) {
-						System.out.println("调用原方法");
+						// System.out.println("调用原方法");
 						res = method.invoke(conn, args);
 					} else {
-						System.out.println("拦截 close 方法");
+						// System.out.println("拦截 close 方法" + pool.size());
 						synchronized (pool) {
-							pool.add((Connection) proxy);
-							System.out.println(pool.size());
-							// todo
-							// connPool.notify();//通知等待线程去获取一个连接
+							res = pool.add((Connection) proxy);
+							// connPool.notify();// todo 通知等待线程去获取一个连接
 						}
 					}
 					return res;
 				});
+	}
 
-
-		// Cglib 方式
-		//		Enhancer enhancer = new Enhancer();
-		//		enhancer.setSuperclass(conn.getClass());
-		//		enhancer.setCallbacks(new Callback[] {
-		//				(MethodInterceptor) (obj, method, args, proxy) -> {
-		//					pool.add((Connection) obj);
-		//					System.out.println("close 被拦截, 连接数" + pool.size());
-		//					return null;
-		//				},
-		//				NoOp.INSTANCE // todo 看 cglib api
-		////				(MethodInterceptor) (obj, method, args, proxy) -> {
-		////					System.out.println("调用原方法, 连接数" + pool.size());
-		////					return proxy.invokeSuper(obj, args);
-		////				}
-		//		});
-		//		enhancer.setCallbackFilter(method -> {
-		//			if (method.getName().equals("close")) {
-		//				return 0;
-		//			} else {
-		//				return 1;
-		//			}
-		//		});
-		//		return (Connection) enhancer.create();
-
-
-
+	/**
+	 * Cglib 方式创建动态代理对象实现逻辑，供工具类接口内部调用
+	 * @param conn
+	 * @param pool
+	 * @return
+	 */
+	private static Connection doProxyByCglib(Connection conn,
+											 final LinkedList<Connection> pool) {
+		Enhancer enhancer = new Enhancer();
+		enhancer.setSuperclass(conn.getClass());
+		enhancer.setCallbacks(new Callback[] {
+				(MethodInterceptor) (obj, method, args, proxy) -> {
+					// System.out.println("close 被拦截, 连接数" + pool.size());
+					return pool.add((Connection) obj);
+				},
+				(MethodInterceptor) (obj, method, args, proxy) -> {
+					// System.out.println("调用原方法, 连接数" + pool.size());
+					return method.invoke(conn, args);
+					// connPool.notify();// todo 通知等待线程去获取一个连接
+				}
+		});
+		enhancer.setCallbackFilter(method -> {
+			if (method.getName().equals("close")) {
+				return 0;
+			} else {
+				return 1;
+			}
+		});
+		return (Connection) enhancer.create();
 	}
 }
 
